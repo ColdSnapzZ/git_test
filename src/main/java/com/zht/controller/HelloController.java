9ef2451e6a3e52fad6ad0c05a3cfd1b4876764fd
torain <2430067402@qq.com>
2020-12-25 09:51:50 +0800

    HelloController

---
+++ src/main/java/com/zht/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.zht.controller;
+
+/**
+ * @Author zht
+ * @Date 2020/12/25 9:49
+ * @Description
+ */
+public class HelloController {
+    public void hello(){
+        System.out.println("hello");
+    }
+}
